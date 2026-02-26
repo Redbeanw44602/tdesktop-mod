06d1fc72a9c47ef127ce4b1a93e41ca8c227d367
Redbeanw44602 <redbeana44945@gmail.com>
2026-02-26 16:13:21 +0800

    Disable invite peeking restrictions.
    Ref: Layerex/telegram-desktop-patches

--- Telegram/SourceFiles/data/data_channel.cpp
+++ Telegram/SourceFiles/data/data_channel.cpp
@@ -1036,7 +1036,7 @@ void ChannelData::clearInvitePeek() {
 }
 
 TimeId ChannelData::invitePeekExpires() const {
-	return _invitePeek ? _invitePeek->expires : 0;
+	return 0;
 }
 
 QString ChannelData::invitePeekHash() const {
